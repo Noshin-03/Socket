6746b71583dd542e2f74152fff1f97972f026b0c
Noshin <bsse1503@iit.du.ac.bd>
2026-02-18 14:40:47 +0600

    updated index.js

--- index.js
+++ index.js
@@ -26,25 +26,48 @@ io.on('connection', (socket) => {
   );
 
   socket.on('set username', (username) => {
-
     if (!socket.recovered) {
       userCount++;
     }
 
     socket.username = username;
 
-    socket.broadcast.emit('chat message', {
-      type: 'system',
-      message: `${username} joined the chat`,
-      count: userCount
-    });
-  });
+    if (socket.username === 'fuad') {
+      io.sockets.sockets.forEach((s) => {
+        if (s.id !== socket.id && s.username !== 'dina') {
+          s.emit('chat message', {
+            type: 'system',
+            message: `${username} joined the chat`,
+            count: userCount
+          });
+        }
+      });
+    } else {
+      socket.broadcast.emit('chat message', {
+        type: 'system',
+        message: `${username} joined the chat`,
+        count: userCount
+      });
+    }
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', {
-      type: 'user',
-      username: socket.username || 'Anonymous',
-      message: msg
+    socket.on('chat message', (msg) => {
+      if (socket.username === 'fuad') {
+        io.sockets.sockets.forEach((s) => {
+          if (s.username !== 'dina') {
+            s.emit('chat message', {
+              type: 'user',
+              username: socket.username || 'Anonymous',
+              message: msg
+            });
+          }
+        });
+      } else {
+        io.emit('chat message', {
+          type: 'user',
+          username: socket.username || 'Anonymous',
+          message: msg
+        });
+      }
     });
   });
 
@@ -62,6 +85,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+server.listen(3003, () => {
+  console.log('Server running at http://localhost:3003');
 });
